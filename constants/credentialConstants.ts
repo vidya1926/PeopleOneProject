5b07699aa3fc810505dbaf1bd522c2b6b5cecdde
majay3574 <majay3574@gmail.com>
2024-06-20 10:54:01 +0530

    Updated project with new features

--- constants/credentialConstants.ts
+++ constants/credentialConstants.ts
@@ -1,6 +1,7 @@
 export enum credentialConstants {
    USERNAME = "admin",
    PASSWORD = "Welcome1@",
+   CUSTOMERADMIN="customadmin",
    LEARNERADMIN="robretcollin@people.co",
    managerUSERNAME ="ajay",
    LEARNERUSERNAME="learneruser1"
