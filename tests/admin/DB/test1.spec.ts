3f128a1fbd7733af8d35fc22c9e33dfa4f881dcd
majay3574 <majay3574@gmail.com>
2024-07-16 10:10:41 +0530

    Updated by Ajay

--- tests/admin/DB/test1.spec.ts
+++ tests/admin/DB/test1.spec.ts
@@ -4,6 +4,11 @@ import { format, addMinutes } from 'date-fns';
 
 
 test('fetch data from database', async () => {
+    test.info().annotations.push(
+        { type: `Author`, description: `Ajay Michael` },
+        { type: `TestCase`, description: `fetch data from database` },
+        { type: `Test Description`, description: `Database` }
+    );
     const dataBase = new DB();
     try {
         const currentTimeResult = await dataBase.executeQuery("SELECT NOW()");
