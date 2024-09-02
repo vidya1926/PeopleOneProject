8c8f296708e8c816a067b1982d589ffa79e92b4a
majay3574 <majay3574@gmail.com>
2024-09-02 11:56:02 +0530

    Updated

--- constants/credentialConstants.ts
+++ constants/credentialConstants.ts
@@ -1,14 +1,14 @@
 import data from "../data/adminGroupsData.json"
 
-const MANAGERNAME:any= data.managerName.toString();
+const MANAGERNAME: any = data.managerName.toString();
 export enum credentialConstants {
    USERNAME = "admin",
    PASSWORD = "Welcome1@",
-   CUSTOMERADMIN="customadmin",
-   LEARNERADMIN="robretcollin@people.co",
-   INSTRUCTORNAME="arivazhaganp",
-   MANAGERUSERNAME =MANAGERNAME,
-   LEARNERUSERNAME="learneruser1",
-   COMMERCEADMIN="commerceadmin"
-   
+   CUSTOMERADMIN = "customadmin",
+   LEARNERADMIN = "robretcollin@people.co",
+   INSTRUCTORNAME = "arivazhaganp",
+   MANAGERUSERNAME = MANAGERNAME,
+   LEARNERUSERNAME = "learneruser1",
+   COMMERCEADMIN = "commerceadmin"
+
 }
